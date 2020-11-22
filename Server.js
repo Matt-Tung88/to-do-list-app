b365080a45aee483d32fc4de7be733d63aa7fc19
Matt-Tung88 <mttw23@gmail.com>
2020-11-21 19:23:48 -0500

    update

--- Server.js
+++ Server.js
@@ -90,7 +90,7 @@ app.post("/todo", async (req, res) => {
 });
 
 const db = mongoose.connection;
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/todo", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
